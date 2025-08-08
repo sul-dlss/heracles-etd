459549220b96bc07b9f7a29495fafc28a2472fb3
Aaron Collier <aaron.collier@stanford.edu>
2025-08-08 12:47:24 -0700

    Add ActiveStorage attachments to model and traits to spec factories

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,6 +2,10 @@
 
 # Model for submissions.
 class Submission < ApplicationRecord
+  has_one_attached :dissertation_file
+  has_many_attached :supplemental_files
+  has_many_attached :permission_files
+
   validates :dissertation_id, presence: true
   validates :druid, presence: true
   validates :etd_type, presence: true, inclusion: { in: %w[Thesis Dissertation] }
