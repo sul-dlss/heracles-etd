abacc9443d59619386319af37d1d5fe0be4f9360
Aaron Collier <aaron.collier@stanford.edu>
2025-08-09 19:10:36 -0700

    Add unique index for email address on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_10_013000) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_10_020940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -351,6 +351,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_10_013000) do
     t.string "orcid"
     t.string "name", null: false
     t.string "first_name", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
