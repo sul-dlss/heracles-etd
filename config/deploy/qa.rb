195b0b305d60a6ae6051db669d5775ed240ed77b
Justin Littman <justinlittman@stanford.edu>
2025-08-11 07:10:32 -0400

    Adds deploy.

---
+++ config/deploy/qa.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+server 'sul-hydra-etd-qa.stanford.edu', user: 'etd', roles: %w[web app db]
---
+++ config/deploy/uat.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+server 'sul-hydra-etd-uat.stanford.edu', user: 'etd', roles: %w[web db app]
