ec10d98a3df2f33e16d430311a3c21290010be9c
Aaron Collier <aaron.collier@stanford.edu>
2025-08-15 16:20:50 -0700

    Remove unused TooltipComponent

--- lib/components/elements/tables/header_component.rb
+++ lib/components/elements/tables/header_component.rb
@@ -4,15 +4,14 @@ module Elements
   module Tables
     # Component for rendering a table header.
     class HeaderComponent < ApplicationComponent
-      def initialize(label:, classes: [], tooltip: nil, style: nil)
+      def initialize(label:, classes: [], style: nil)
         @label = label
         @classes = classes
-        @tooltip = tooltip
         @style = style
         super()
       end
 
-      attr_reader :label, :tooltip, :style
+      attr_reader :label, :style
 
       def classes
         merge_classes(@classes)
