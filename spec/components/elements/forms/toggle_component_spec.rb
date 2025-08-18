c6e6fcabfb69739d7d6709ca65494983542507fd
Aaron Collier <aaron.collier@stanford.edu>
2025-08-18 14:20:35 -0700

    Add test coverage for edited component

---
+++ spec/components/elements/forms/toggle_component_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Elements::Forms::ToggleComponent, type: :component do
+  let(:submission) { create(:submission, name: 'Doe, Jane') }
+  let(:form) { ActionView::Helpers::FormBuilder.new(nil, submission, vc_test_controller.view_context, {}) }
+  let(:field_name) { 'supplemental_files_uploaded' }
+  let(:label) { 'Supplemental Files' }
+  let(:container_classes) { 'mb-4' }
+  let(:component) { described_class.new(form:, field_name:, label:, container_classes:) }
+
+  before do
+    component.with_left_toggle_option(form:, field_name:, label: 'Yes', value: true, data: { test: 'test_data' })
+    component.with_right_toggle_option(form:, field_name:, label: 'No', value: false, data: { test: 'more_test_data' })
+  end
+
+  it 'creates toggle field with label' do
+    render_inline(component)
+    expect(page).to have_css('label.form-label', text: 'Supplemental Files')
+    expect(page).to have_css('input[data-test="test_data"]')
+    expect(page).to have_css('label.btn.rounded-start-pill', text: 'Yes')
+    expect(page).to have_css('label.btn.rounded-end-pill', text: 'No')
+    expect(page).to have_no_css('p.form-text')
+  end
+end
