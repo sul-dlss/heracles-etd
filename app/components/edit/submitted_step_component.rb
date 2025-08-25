584c9c93a935ce86cb9da0a1f7e4f5ca6c8486a1
Aaron Collier <aaron.collier@stanford.edu>
2025-08-25 15:09:50 -0700

    Remove the button wrapper for the review and submit link

--- app/components/edit/submitted_step_component.rb
+++ app/components/edit/submitted_step_component.rb
@@ -18,5 +18,20 @@ module Edit
     def step_range_end
       SubmissionPresenter.total_steps - 1
     end
+
+    def review_link
+      link_to(review_submission_path(@submission.dissertation_id),
+              class: ComponentSupport::ButtonSupport.classes(variant: 'primary', classes:)) do
+        'Review and submit'
+      end
+    end
+
+    private
+
+    def classes
+      return if all_done?
+
+      'disabled'
+    end
   end
 end
