d3d87c9c0b9f9fef5016d1f68159daf4b3528ff3
Aaron Collier <aaron.collier@stanford.edu>
2025-08-27 15:59:53 -0700

    Update mail config and establish settings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "Etd App <#{Settings.etd_alerts_list}>",
+          to: Settings.etd_alerts_list
   layout 'mailer'
 end
