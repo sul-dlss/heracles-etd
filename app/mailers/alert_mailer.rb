f354e3e904cab8b5036fc0a5a7c457c1671e69d6
Aaron Collier <aaron.collier@stanford.edu>
2025-08-29 12:33:25 -0700

    Only maintain the cataloging based alert mailer in favor of HB

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -2,48 +2,6 @@
 
 # Mailer for sending alerts related to ETD submissions
 class AlertMailer < ApplicationMailer
-  def ps_submit_exception(dissertation_id, exception_msg)
-    @id = dissertation_id
-    @msg = exception_msg
-    send_message(subject: "#{subject_prefix} Error submitting ETD information to PeopleSoft",
-                 template_name: 'ps_submit_exception')
-  end
-
-  def ps_incoming_dissertation_exception(dissertation_id, exception_msg)
-    @id = dissertation_id
-    @msg = exception_msg
-    send_message(subject: "#{subject_prefix} Error processing incoming dissertation from Peoplesoft",
-                 template_name: 'ps_incoming_dissertation_exception')
-  end
-
-  def corrupt_pdf_notification(dissertation_id, exception_msg)
-    @id = dissertation_id
-    @msg = exception_msg
-    send_message(subject: "#{subject_prefix} Error creating augmented pdf",
-                 template_name: 'corrupt_pdf_notification')
-  end
-
-  def readers_missing(dissertation_id)
-    @id = dissertation_id
-    send_message(subject: "#{subject_prefix} Dissertation is missing readers",
-                 template_name: 'readers_missing')
-  end
-
-  def unable_to_create_workflow(dissertation_id)
-    send_workflow_message('create', dissertation_id)
-  end
-
-  def unable_to_update_workflow(dissertation_id)
-    send_workflow_message('update', dissertation_id)
-  end
-
-  def send_workflow_message(action, dissertation_id)
-    @id = dissertation_id
-    @action = action
-    send_message(subject: "#{subject_prefix} Error with Etd Submit workflow #{action}",
-                 template_name: 'workflow_problem')
-  end
-
   def ready_for_cataloging
     # ETDs loaded since yesterday morning
     @etds_uncataloged_new = Submission.ils_records_created_since_yesterday_morning.order(:folio_instance_hrid)
