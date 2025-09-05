c2e3dfb64635f22dc67420f41d8e1ef03ceaeb24
Aaron Collier <aaron.collier@stanford.edu>
2025-09-05 14:19:23 -0700

    Add CatalogStatusJob and schedule to occur every hour (#199)

---
+++ app/jobs/catalog_status_job.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Job to update catalog status of ETDs and trigger accessioning when ready
+class CatalogStatusJob < ApplicationJob
+  def perform
+    Honeybadger.check_in(Settings.honeybadger_checkins.catalog_status)
+
+    Submission.has_catalog_record_id.ils_record_not_updated.find_each do |submission|
+      next unless FolioChecker.cataloged?(submission:)
+
+      Submission.transaction do
+        submission.update!(ils_record_updated_at: Time.zone.now)
+        StartAccessionJob.perform_later(submission.druid)
+      end
+    end
+  end
+end
