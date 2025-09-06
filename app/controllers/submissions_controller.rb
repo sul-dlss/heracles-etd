41904a1d844666442a85daca58fd684b4a9a4067
Aaron Collier <aaron.collier@stanford.edu>
2025-09-05 17:27:24 -0700

    Update interaction and text for permission files step

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -18,7 +18,7 @@ class SubmissionsController < ApplicationController
       @submission.supplemental_files.find(params[:submission][:remove_supplemental_file]).delete
     elsif params.dig(:submission, :remove_permission_file)
       @submission.permission_files.find(params[:submission][:remove_permission_file]).purge
-      @submission.update!(permissions_provided: 'false') if @submission.permission_files.empty?
+      @submission.update!(permissions_provided: nil) if @submission.permission_files.empty?
     else
       @submission.update!(submission_params)
     end
