70cd949f2391a74d4bf341a5d40693cda790758f
Justin Littman <justinlittman@stanford.edu>
2025-09-09 13:03:53 -0400

    Tweak supplemental file fixture with diacritic. (#228)

--- spec/factories/supplemental_files.rb
+++ spec/factories/supplemental_files.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     # Yes, this supplemental filename has a composed diacritic, so we test that they work.
     trait :first_supplement do
       description { 'Supplemental file supplémental_1.pdf' }
-      file { Rack::Test::UploadedFile.new(file_fixture('supplémental_1.pdf'), 'application/pdf') }
+      file { Rack::Test::UploadedFile.new(file_fixture('supplemental_1.pdf'), 'application/pdf', original_filename: 'supplémental_1.pdf') }
     end
 
     trait :second_supplement do
