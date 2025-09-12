7c310ccaffe916bbbdc3e69a8e5acaf2a8b577ec
Aaron Collier <aaron.collier@stanford.edu>
2025-09-12 15:17:09 -0700

    Refactor migration into job

---
+++ app/jobs/active_storage_migration_job.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Job to migrate legacy file attachments to ActiveStorage for a specific submission
+# NOTE: This job can be removed once all legacy attachments have been migrated
+class ActiveStorageMigrationJob < ApplicationJob
+  queue_as :default
+
+  def perform(submission_id)
+    submission = Submission.find(submission_id)
+    legacy_submission = ActiveStorage::LegacySubmission.new(submission:)
+
+    ActiveStorage::MigratorService.migrate(legacy_submission:)
+  end
+end
