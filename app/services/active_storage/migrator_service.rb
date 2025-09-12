2bd1131ee2b1544cdf77dcc83972c69382e1da49
Aaron Collier <aaron.collier@stanford.edu>
2025-09-12 15:17:09 -0700

    Refactor migration into job

--- app/services/active_storage/migrator_service.rb
+++ app/services/active_storage/migrator_service.rb
@@ -4,49 +4,66 @@ module ActiveStorage
   # Service to handle migration of legacy file attachments to ActiveStorage
   # This service can be called multiple times safely
   class MigratorService
-    WORKSPACE_DIR = '/opt/app/etd/workspace'
-
-    def self.migrate_all(logger:)
-      new(logger: logger).migrate_all
+    def self.migrate(legacy_submission:)
+      new(legacy_submission:).migrate
     end
 
-    def initialize(logger:)
-      @logger = logger
+    def initialize(legacy_submission:)
+      @legacy_submission = legacy_submission
     end
 
-    attr_reader :logger
+    attr_reader :legacy_submission
 
-    def migrate_all
-      logger.info "Starting migration of legacy attachments at #{Time.current}"
-      logger.info "Using workspace directory: #{WORKSPACE_DIR}"
-      logger.info "Found #{legacy_submissions.count} submissions to process"
-      legacy_submissions.each do |legacy_submission|
-        logger.info "Processing submission #{legacy_submission.id}, dissertation_id: #{legacy_submission.druid}"
-        # Migrate permission files first
-        # Permission files
-        if legacy_submission.permission_files.none?
-          logger.info "No permission files found for submission #{legacy_submission.id}"
-        end
-      end
+    delegate :submission, :permission_files, :supplemental_files,
+             :dissertation_file, :augmented_dissertation_file, to: :legacy_submission
+
+    def migrate
+      migrate_permission_files
+      migrate_supplemental_files
+      migrate_dissertation_file
     end
 
     private
 
-    # Returns all submissions that do not have an ActiveStorage attachment for the dissertation file
-    def legacy_submissions
-      @legacy_submissions ||= Submission.left_joins(:dissertation_file_attachment)
-                                        .where(active_storage_attachments: { id: nil })
-                                        .map { |submission| ActiveStorage::LegacySubmission.new(submission:) }
+    # Migrate permission files first
+    # Permission files
+    def migrate_permission_files
+      return if permission_files.none?
+
+      permission_files.map { |file_params| migrate_file_attachment(file_type: PermissionFile, file_params:) }
     end
 
-    def legacy_file_path(file_name)
-      File.join(WORKSPACE_DIR, submission.druid, file_name)
+    # Migrate supplemental files
+    def migrate_supplemental_files
+      return if supplemental_files.none?
+
+      supplemental_files.map { |file_params| migrate_file_attachment(file_type: SupplementalFile, file_params:) }
     end
 
-    def attach_file(file_record, file_path)
-      file_record.file.attach(io: File.open(file_path), filename: file_name)
-    rescue Errno::ENOENT => e
-      logger.error "File not found: #{file_path} for submission #{submission.id}. Error: #{e.message}"
+    # Migrate dissertation file
+    def migrate_dissertation_file
+      return if dissertation_file.blank?
+
+      attach_augmented_dissertation_file
+      attach_dissertation_file
+    end
+
+    def attach_dissertation_file
+      # Attaches the dissertation file to the submission
+      submission.dissertation_file.attach(io: File.open(dissertation_file[:file_path]),
+                                          filename: dissertation_file[:file_name])
+    end
+
+    def attach_augmented_dissertation_file
+      # Attaches the augmented dissertation file to the submission
+      submission.augmented_dissertation_file.attach(io: File.open(augmented_dissertation_file[:file_path]),
+                                                    filename: augmented_dissertation_file[:file_name])
+    end
+
+    def migrate_file_attachment(file_type:, file_params:)
+      file_record = file_type.new(submission:, description: file_params[:label])
+      file_record.file.attach(io: File.open(file_params[:file_path]), filename: file_params[:file_name])
+      file_record.save!
     end
   end
 end
