8931d27e12394269a548534fb00908aa7df4c8a4
Aaron Collier <aaron.collier@stanford.edu>
2025-09-16 11:13:16 -0700

    Refactor away form job to run synchronously with output

--- app/services/active_storage/migrator_service.rb
+++ app/services/active_storage/migrator_service.rb
@@ -4,16 +4,17 @@ module ActiveStorage
   # Service to handle migration of legacy file attachments to ActiveStorage
   # This service can be called multiple times safely
   class MigratorService
-    def self.migrate(dissertation_file:)
-      new(dissertation_file:).migrate
+    class MissingDissertationFileError < StandardError; end
+
+    def self.migrate(submission:)
+      new(submission:).migrate
     end
 
-    def initialize(dissertation_file:)
-      @dissertation_file = dissertation_file
-      @submission = dissertation_file.submissions.first
+    def initialize(submission:)
+      @submission = submission
     end
 
-    attr_reader :dissertation_file, :submission
+    attr_reader :submission
 
     delegate :legacy_parts, to: :submission
 
@@ -52,27 +53,40 @@ module ActiveStorage
     end
 
     # Migrate dissertation file
+    def legacy_dissertation_file
+      legacy_parts.find { |part| part.is_a?(LegacyDissertationFile) }
+    end
+
     def migrate_dissertation_file
       attach_augmented_dissertation_file
       attach_dissertation_file
     end
 
     def attach_dissertation_file
+      raise MissingDissertationFileError unless legacy_dissertation_file
+
       # Attaches the dissertation file to the submission
-      submission.dissertation_file.attach(io: File.open(dissertation_file.file_path),
-                                          filename: dissertation_file.file_name)
+      submission.dissertation_file.attach(io: File.open(legacy_dissertation_file.file_path),
+                                          filename: legacy_dissertation_file.file_name)
     end
 
     def attach_augmented_dissertation_file
+      return unless legacy_dissertation_file
+
       # Attaches the augmented dissertation file to the submission
-      submission.augmented_dissertation_file.attach(io: File.open(dissertation_file.augmented_path),
-                                                    filename: dissertation_file.augmented_file_name)
+      submission.augmented_dissertation_file.attach(io: File.open(legacy_dissertation_file.augmented_path),
+                                                    filename: legacy_dissertation_file.augmented_file_name)
     end
 
+    # This attempts to attach a file and logs an error if it fails
+    # but continues processing other files as this is only used for
+    # the optional Permission and Supplemental files
     def migrate_file_attachment(file_type:, file:)
       file_record = file_type.new(submission:, description: file.label)
       file_record.file.attach(io: File.open(file.file_path), filename: file.file_name)
-      file_record.save!
+    rescue StandardError => e
+      Rails.logger.error "Failed to migrate #{file.file_name} for #{submission.druid}: #{e.message}"
+      file_record.destroy
     end
   end
 end
--- spec/services/active_storage/migrator_service_spec.rb
+++ spec/services/active_storage/migrator_service_spec.rb
@@ -4,22 +4,25 @@ require 'rails_helper'
 
 RSpec.describe ActiveStorage::MigratorService do
   let(:submission) { create(:submission) }
-  let(:dissertation_file) { submission.legacy_parts.find { |part| part.is_a?(LegacyDissertationFile) } }
-
-  before do
-    create(:attachment, :with_legacy_dissertation_file, submission:)
-    FileUtils.mkdir_p(File.dirname(dissertation_file.file_path))
-  end
-
-  after do
-    FileUtils.rm_r(File.dirname(File.join(Settings.file_uploads_root, submission.druid)))
-  end
 
   describe '.migrate' do
+    before do
+      FileUtils.mkdir_p(File.join(Settings.file_uploads_root, submission.druid))
+    end
+
+    after do
+      FileUtils.rm_r(File.join(Settings.file_uploads_root, submission.druid))
+    end
+
     context 'when migrating a legacy ETD' do
+      let(:legacy_dissertation_file) { submission.legacy_parts.find { |part| part.is_a?(LegacyDissertationFile) } }
+
       before do
-        FileUtils.cp(file_fixture('dissertation.pdf'), dissertation_file.file_path)
-        FileUtils.cp(file_fixture('dissertation-augmented.pdf'), dissertation_file.augmented_path)
+        create(:attachment, :with_legacy_dissertation_file, submission:)
+
+        submission.reload
+        FileUtils.cp(file_fixture('dissertation.pdf'), legacy_dissertation_file.file_path)
+        FileUtils.cp(file_fixture('dissertation-augmented.pdf'), legacy_dissertation_file.augmented_path)
       end
 
       context 'when all file types are attached and present' do
@@ -40,12 +43,38 @@ RSpec.describe ActiveStorage::MigratorService do
           expect(submission.augmented_dissertation_file).not_to be_attached
           expect(submission.supplemental_files.count).to eq 0
           expect(submission.permission_files.count).to eq 0
-          described_class.migrate(dissertation_file:)
+          described_class.migrate(submission:)
           submission.reload
           expect(submission.dissertation_file).to be_attached
           expect(submission.augmented_dissertation_file).to be_attached
           expect(submission.supplemental_files.count).to eq 1
           expect(submission.permission_files.count).to eq 1
+          expect(ActiveStorage::Attachment.count).to eq 4
+        end
+      end
+
+      context 'when supplemental and permissions files are expected but not found on disk' do
+        let(:supplemental_file) { submission.legacy_parts.find { |part| part.is_a?(LegacySupplementalFile) } }
+        let(:permission_file) { submission.legacy_parts.find { |part| part.is_a?(LegacyPermissionFile) } }
+
+        before do
+          create(:attachment, :with_legacy_supplemental_file, submission:)
+          create(:attachment, :with_legacy_permission_file, submission:)
+          submission.reload
+        end
+
+        it 'migrates successfully' do
+          expect(submission.dissertation_file).not_to be_attached
+          expect(submission.augmented_dissertation_file).not_to be_attached
+          expect(submission.supplemental_files.count).to eq 0
+          expect(submission.permission_files.count).to eq 0
+          described_class.migrate(submission:)
+          submission.reload
+          expect(submission.dissertation_file).to be_attached
+          expect(submission.augmented_dissertation_file).to be_attached
+          expect(submission.supplemental_files.count).to eq 0
+          expect(submission.permission_files.count).to eq 0
+          expect(ActiveStorage::Attachment.count).to eq 2
         end
       end
 
@@ -55,12 +84,13 @@ RSpec.describe ActiveStorage::MigratorService do
           expect(submission.augmented_dissertation_file).not_to be_attached
           expect(submission.supplemental_files.count).to eq 0
           expect(submission.permission_files.count).to eq 0
-          described_class.migrate(dissertation_file:)
+          described_class.migrate(submission:)
           submission.reload
           expect(submission.dissertation_file).to be_attached
           expect(submission.augmented_dissertation_file).to be_attached
           expect(submission.supplemental_files.count).to eq 0
           expect(submission.permission_files.count).to eq 0
+          expect(ActiveStorage::Attachment.count).to eq 2
         end
       end
     end
@@ -79,11 +109,12 @@ RSpec.describe ActiveStorage::MigratorService do
       end
 
       it 'raises an error and rolls back attaching any files' do
-        expect { described_class.migrate(dissertation_file:) }.to raise_error(Errno::ENOENT)
+        expect { described_class.migrate(submission:) }.to raise_error(ActiveStorage::MigratorService::MissingDissertationFileError)
         expect(submission.dissertation_file).not_to be_attached
         expect(submission.augmented_dissertation_file).not_to be_attached
         expect(submission.supplemental_files.count).to eq 0
         expect(submission.permission_files.count).to eq 0
+        expect(ActiveStorage::Attachment.count).to eq 0
       end
     end
   end
