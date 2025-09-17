3558e4a2de20b9074f0242d745c5e003639afa46
Justin Littman <justinlittman@stanford.edu>
2025-09-17 08:14:34 -0400

    Warn when abstract contains formatting.
    closes #309

--- app/javascript/controllers/abstract_controller.js
+++ app/javascript/controllers/abstract_controller.js
@@ -1,13 +1,27 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['doneButton']
+  static targets = ['doneButton', 'warning', 'input']
 
-  toggleButton (event) {
-    if (event.target.value.trim().length > 0) {
+  toggleButton () {
+    if (this.abstract.length > 0) {
       this.doneButtonTarget.disabled = false
     } else {
       this.doneButtonTarget.disabled = true
     }
+  }
+
+  warnFormatting () {
+    const warn = this.abstract.match(/\$.+\$/) || // e.g., $\sim 100\gev$-$1\tev$
+      this.abstract.match(/\\[a-zA-Z]+\{.+\}/) // e.g., \cite{p-Jungman:1995df}
+    this.warningTarget.classList.toggle('d-none', !warn)
+  }
+
+  get abstract () {
+    return this.inputTarget.value.trim()
+  }
+
+  inputTargetConnected () {
+    this.warnFormatting()
   }
 }
