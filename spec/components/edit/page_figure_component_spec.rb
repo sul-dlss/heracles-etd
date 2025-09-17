ffaf23e5dac43dd4bad76332b8b58418aefda723
Justin Littman <justinlittman@stanford.edu>
2025-09-17 15:09:52 -0400

    Adds aria-hidden to page figures.
    closes #321

--- spec/components/edit/page_figure_component_spec.rb
+++ spec/components/edit/page_figure_component_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Edit::PageFigureComponent, type: :component do
   it 'renders the page figure' do
     render_inline(described_class.new(title: 'Sample Title', with_x: true, footer: 'Sample Footer'))
-    expect(page).to have_css('.border-1 small', text: 'Sample Title')
+    expect(page).to have_css('.border-1[aria-hidden="true"] small', text: 'Sample Title')
     expect(page).to have_css('.bottom-0.start-50', text: 'Sample Footer')
     expect(page).to have_css('.bi-x-circle')
   end
