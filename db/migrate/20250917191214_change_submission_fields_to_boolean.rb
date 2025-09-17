e5a0b328093d2374b1818cc07f17c1543cc65ae7
Justin Littman <justinlittman@stanford.edu>
2025-09-17 16:28:02 -0400

    Change permission_files_uploaded field to boolean.

--- db/migrate/20250917191214_change_submission_fields_to_boolean.rb
+++ db/migrate/20250917191214_change_submission_fields_to_boolean.rb
@@ -1,20 +1,10 @@
 class ChangeSubmissionFieldsToBoolean < ActiveRecord::Migration[8.0]
-  FIELDS = %i[citation_verified abstract_provided dissertation_uploaded].freeze
+  FIELDS = %i[citation_verified abstract_provided dissertation_uploaded supplemental_files_uploaded permission_files_uploaded].freeze
   def up
-    # add_column :submissions, :citation_verified_bool, :boolean, default: false, null: false
-    # Submission.reset_column_information
-    # Submission.where(citation_verified: 'true').update_all(citation_verified_bool: true)
-    # remove_column :submissions, :citation_verified
-    # rename_column :submissions, :citation_verified_bool, :citation_verified
     FIELDS.each { |field| column_str_to_bool(field) }
   end
 
   def down
-    # add_column :submissions, :citation_verified_str, :string, default: nil
-    # Submission.reset_column_information
-    # Submission.where(citation_verified: true).update_all(citation_verified_str: 'true')
-    # remove_column :submissions, :citation_verified
-    # rename_column :submissions, :citation_verified_str, :citation_verified
     FIELDS.each { |field| column_bool_to_str(field) }
   end
 
@@ -35,6 +25,4 @@ class ChangeSubmissionFieldsToBoolean < ActiveRecord::Migration[8.0]
     remove_column :submissions, column
     rename_column :submissions, temp_column, column
   end
-
-  # abstract_provided
 end
--- db/schema.rb
+++ db/schema.rb
@@ -113,9 +113,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_18_225521) do
     t.string "regactiondttm"
     t.string "documentaccess"
     t.string "submit_date"
-    t.string "supplemental_files_uploaded"
     t.string "permissions_provided"
-    t.string "permission_files_uploaded"
     t.string "rights_selected"
     t.string "cc_license_selected"
     t.string "submitted_to_registrar"
@@ -135,6 +133,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_18_225521) do
     t.boolean "citation_verified", default: false, null: false
     t.boolean "abstract_provided", default: false, null: false
     t.boolean "dissertation_uploaded", default: false, null: false
+    t.boolean "supplemental_files_uploaded", default: false, null: false
+    t.boolean "permission_files_uploaded", default: false, null: false
     t.index ["dissertation_id"], name: "index_submissions_on_dissertation_id", unique: true
     t.index ["druid"], name: "index_submissions_on_druid", unique: true
   end
